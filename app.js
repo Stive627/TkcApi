1522354e277facc502ef43f23ea16b7132862b58
Stive627 <fossistive627@gmail.com>
2025-03-21 23:34:54 +0530

    solved merge problem

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ require('dotenv').config()
 const app = express()
 const port = process.env.port || 8080
 app.use(express.static("public"))
+app.get('/', (req, res) => res.status(200).send('The Api is working well!'))
 app.listen(port, ()=>console.log(`The server is running at http://localhost:${port}`))
