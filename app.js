5e165a7ba8389916d52a871a18125544d8f7fbb7
Stive627 <fossistive627@gmail.com>
2025-03-30 23:44:37 +0530

    cors corrected

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 const app = express()
 const port = process.env.port || 8080
 const uri = process.env.uri
-app.use(cors({origin:'http://localhost:3000', credentials:'true'}))
+app.use(cors())
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
