67ac482b3790149eb9c467d3fb991f2e7d31b6bc
Stive627 <fossistive627@gmail.com>
2025-04-02 21:56:10 +0530

    second commit

--- Authentication/controllerAuthentication.js
+++ Authentication/controllerAuthentication.js
@@ -41,7 +41,7 @@ const login = async (req, res) => {
     const goodPassowrd = await bcrypt.compare(password, user.password).catch((error)=>res.status(400).send(error))
     if(goodPassowrd){
         res.cookie('logininfo',JSON.stringify({nameoremail:usernameoremail, password:password}), {maxAge:1000*60*60*24*30, httpOnly:true, SameSite:'None'})
-        return res.redirect(`https://tkc.tsasoft.com/?${usernameoremail}`)  
+        return res.redirect(`https://tkc.tsasoft.com/?email=${usernameoremail}`)  
     }
     res.status(400).send('Invalid credentials. Please double check and enter. the correct credentials')
 }
